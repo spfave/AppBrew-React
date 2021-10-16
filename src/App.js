2bf75074dc72f97c949590cdc76b30569eb3e635
Sebastian F <sebpfave2@gmail.com>
2021-10-16 18:54:21 -0400

    Create base keeper app components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Note from './components/Note';
+
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
 }
---
+++ src/components/Note.jsx
@@ -0,0 +1,8 @@
+export default function Note() {
+  return (
+    <div className="note">
+      <h1>Note title</h1>
+      <p>Note content</p>
+    </div>
+  );
+}
